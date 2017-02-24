1f3cc6322f978833bc95f9462b1e2ec629587918
Renee Mei <meiqianye@gmail.com>
2017-02-24 11:06:26 -0800

    add test for testing empty stylist table at beginning

---
+++ Objects/Stylists.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace HairSalon
+{
+  public class Stylist
+  {
+    
+  }
+}
---
+++ Tests/StylistsTest.cs
@@ -0,0 +1,24 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace HairSalon
+{
+  public class StylistTest : IDisposable
+  {
+    public StylistTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
+    }
+
+    [Fact]
+    public void Test_StylistTableEmptyAtFirst()
+    {
+      int result = Stylist.GetAll().Count;
+
+      Assert.Equal(0, result);
+    }
+  }
+}
